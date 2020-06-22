81fd85bbd81a86b1a521129fb5dc5c9285aa0fa6
Yash Kapoor <kapooryash713@gmail.com>
2020-06-22 19:20:48 +0530

    Snake and Food

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Snake from './Snake';
+import Food from './Food';
+
 
 class App extends Component {
+  state={
+    food: [6,8],
+    snakeDots:[
+      [0,0],
+      [2,0]
+  ]
+}
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="game-area">
+        <Snake snakeDots={this.state.snakeDots}/>
+        <Food dot={this.state.food}/>
+       
       </div>
     );
   }
